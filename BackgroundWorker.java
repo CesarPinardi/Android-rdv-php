cfaf6e3d76d73d6bbff3453ec330c0c90abfa629
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-02-01 16:07:53 -0300

    Update BackgroundWorker.java

--- BackgroundWorker.java
+++ BackgroundWorker.java
@@ -29,43 +29,9 @@ public class BackgroundWorker extends AsyncTask <String, Void, String> {
 
         /*String do banco da vet: */
 
-        String login_url = "http://192.168.0.58/rdv/app/login_rdv.php";
-        String movimento_url = "http://192.168.0.58/rdv/app/movimento_rdv.php";
+        String movimento_url = "http://192.168.5.234/rdv/app/movimento_rdv.php";
 
-        if(type.equals("login")) {
-            try {
-                String user_name = params[1];
-                String password = params[2];
-                URL url = new URL(login_url);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
-
-                OutputStream outputStream = httpURLConnection.getOutputStream();
-                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
-                String post_data = URLEncoder.encode("user_name","UTF-8")+"="+URLEncoder.encode(user_name,"UTF-8")+"&"
-                        +URLEncoder.encode("password","UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
-                bufferedWriter.write(post_data);
-                bufferedWriter.flush();
-                bufferedWriter.close();
-                outputStream.close();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream,"iso-8859-1"));
-                String result="";
-                String line="";
-                while((line = bufferedReader.readLine())!= null) {
-                    result += line;
-                }
-                bufferedReader.close();
-                inputStream.close();
-                httpURLConnection.disconnect();
-                return result;
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if(type.equals("regMov")) {
+        if(type.equals("regMov")) {
             try {
                 String id_func = params[1];
                 String id_desp = params[2];

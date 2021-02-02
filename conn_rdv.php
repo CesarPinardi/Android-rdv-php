907dd48ac4f66af6083ede6c55374d4d820239cc
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-02-02 15:54:14 -0300

    Delete conn_rdv.php

--- conn_rdv.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_name="rdv";
-$mysql_username="root";
-$mysql_password="";
-$server_name="localhost";
-$conn_rdv = mysqli_connect($server_name,$mysql_username,$mysql_password,$db_name);
-    
-
-?>

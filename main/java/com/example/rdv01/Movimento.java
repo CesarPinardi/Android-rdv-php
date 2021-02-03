8a3d0264a9934afcd6545ad1df82309f2535b90f
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-02-03 16:14:52 -0300

    Update Movimento.java

--- main/java/com/example/rdv01/Movimento.java
+++ main/java/com/example/rdv01/Movimento.java
@@ -6,6 +6,9 @@ import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -22,17 +25,19 @@ import org.json.JSONObject;
 
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.sql.Connection;
 import java.util.Calendar;
 
 public class Movimento extends AppCompatActivity implements View.OnClickListener{
-    private static final String FILE_NAME = "movimento";
     EditText  valor_desp, valor_km, obs;
     TextView id_func, id_desp, labelkm;
 
     /* declaracoes para o calendario */
-    Button btnDatePicker;
+    Button btnDatePicker, btnInsert, btnStore;
     EditText txtDate;
     private int mYear, mMonth, mDay;
 
@@ -49,6 +54,9 @@ public class Movimento extends AppCompatActivity implements View.OnClickListener
         id_desp = findViewById(R.id.tvResult);
 
         btnDatePicker = findViewById(R.id.btn_date);
+        btnInsert = findViewById(R.id.btnInsertMov);
+        btnStore = findViewById(R.id.btnGuardaMov);
+
         txtDate = findViewById(R.id.in_date);
 
         labelkm = findViewById(R.id.labelKm);
@@ -116,33 +124,110 @@ public class Movimento extends AppCompatActivity implements View.OnClickListener
 
     }
 
-    public void OnGuardarMovimento(View view) {
+    public JSONObject OnGuardarMovimento(View view) throws IOException {
         /*aqui guardar em um json para envio futuro*/
+
+        JSONObject jObj = new JSONObject();
+
+        try {
+            jObj.put("id_func", id_func.getText().toString());
+            jObj.put("id_desp", id_desp.getText().toString());
+            jObj.put("valor_desp", valor_desp.getText().toString());
+            jObj.put("valor_km", valor_km.getText().toString());
+            jObj.put("obs", obs.getText().toString());
+            jObj.put("dataM", txtDate.getText().toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(jObj);
+
+        String strJson = jObj.toString();
+        FileOutputStream fos = null;
+
+        final String FILE_NAME = id_func.getText().toString() + "  Data  " + txtDate.getText().toString();
+        
+        try {
+            fos = openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(strJson.getBytes());
+
+            Toast.makeText(this, "Salvo em  " + getFilesDir() + "/" + FILE_NAME,
+                    Toast.LENGTH_LONG).show();
+            
+            // Toast para depois dos testes:
+            // Toast.makeText(this, "Despesa salva!" , Toast.LENGTH_LONG).show();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return jObj;
 
     }
 
+    public boolean isNetworkAvailable(){
+        try {
+            ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = null;
+
+            if(manager != null){
+                networkInfo = manager.getActiveNetworkInfo();
+            }
+            return networkInfo != null && networkInfo.isConnected();
+        } catch (NullPointerException e){
+            return false;
+        }
+    }
+
+
     public void OnEnviarMovimento(View view) {
 
-        AlertDialog alerta;
+        if(!isNetworkAvailable()){
 
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            AlertDialog alerta;
 
-        builder.setMessage("Deseja enviar os dados?");
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
 
-        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-                /*Função que prepara para o envio dos dados*/
-                callBackground();
-            }
-        });
+            builder.setTitle("Dispositivo não conectado!").
+            setMessage("Conecte-se à uma rede e reinicie a aplicação para tentar novamente...\n\nOu então escolha a opção 'Guardar'.");
+            alerta = builder.create();
+            alerta.show();
+            btnInsert.setClickable(false);
+            btnInsert.setBackgroundColor(Color.DKGRAY);
 
-        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface arg0, int arg1) {
-                Toast.makeText(Movimento.this, "Dados não enviados" + arg1, Toast.LENGTH_SHORT).show();
-            }
-        });
-        alerta = builder.create();
-        alerta.show();
+        } else {
+            AlertDialog alerta;
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+            builder.setMessage("Deseja enviar os dados?");
+
+            builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface arg0, int arg1) {
+                    /*Função que prepara para o envio dos dados*/
+                    callBackground();
+                }
+            });
+
+            builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface arg0, int arg1) {
+                    Toast.makeText(Movimento.this, "Dados não enviados" + arg1, Toast.LENGTH_SHORT).show();
+                }
+            });
+            alerta = builder.create();
+            alerta.show();
+        }
+
     }
 
     private void callBackground() {

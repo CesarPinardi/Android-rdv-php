eccf4c4e0a30e542e1b7ae59a3588266bfa8b3f0
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-02-09 16:55:43 -0300

    agora com "mostrar senha"

--- main/java/com/example/rdv01/MainActivity.java
+++ main/java/com/example/rdv01/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.rdv01;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -21,9 +25,9 @@ import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
-    EditText UsernameEt, PasswordEt;
-    Button login;
-
+    EditText userEt, passEt;
+    Button login, limpar;
+    CheckBox checkbox;
     private RequestQueue requestQueue;
     /*String para o banco*/
     private static final String URL = "http://192.168.0.126/rdv/app/user_control.php";
@@ -33,9 +37,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        UsernameEt = findViewById(R.id.etUserName);
-        PasswordEt = findViewById(R.id.etPassword);
+        userEt = findViewById(R.id.etUserName);
+        passEt = findViewById(R.id.etPassword);
         login = findViewById(R.id.btnLogin);
+        limpar = findViewById(R.id.btnLimpar);
+        checkbox = findViewById(R.id.checkBox);
+
+        checkbox.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+            if (isChecked) {
+                // mostra password
+                passEt.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            } else {
+                // esconde password
+                passEt.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            }
+        });
+
+        limpar.setOnClickListener(v->{
+            userEt.setText("");
+            passEt.setText("");
+        });
 
         /*Intent para ir para a outra activity*/
         Intent i = new Intent(MainActivity.this,Movimento.class);
@@ -51,13 +72,15 @@ public class MainActivity extends AppCompatActivity {
 
                         Bundle params = new Bundle();
                         /*Enviar o valor do editText para a próxima activity*/
-                        params.putString("loginUser", UsernameEt.getText().toString());
+                        params.putString("loginUser", userEt.getText().toString());
                         i.putExtras(params);
                         /*Assim que passar, vai para a outra activity*/
                         startActivity(i);
 
                     } else {
-                        Toast.makeText(getApplicationContext(), "Error" + jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            if(Objects.requireNonNull(jsonObject.names()).get(0).equals("error")){
+                                Toast.makeText(getApplicationContext(), "Error" + jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
+                            }
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
@@ -69,8 +92,8 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 protected Map<String, String> getParams() {
                     HashMap<String,String> hashMap = new HashMap<>();
-                    hashMap.put("username", UsernameEt.getText().toString());
-                    hashMap.put("password", PasswordEt.getText().toString());
+                    hashMap.put("username", userEt.getText().toString());
+                    hashMap.put("password", passEt.getText().toString());
                     return hashMap;
                 }
             };

399bbdfc8e37cd3254f50b586dd75b0406616521
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-02-25 16:43:31 -0300

    Update MainActivity.java

--- main/java/com/example/rdv01/MainActivity.java
+++ main/java/com/example/rdv01/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.rdv01;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.preference.PreferenceManager;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.widget.Button;
@@ -10,6 +13,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -25,22 +29,31 @@ import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     EditText userEt, passEt;
-    Button login, limpar;
+    Button login;
+    Button limpar;
     CheckBox checkbox;
+
     private RequestQueue requestQueue;
-    /*String para o banco*/
-    private static final String URL = "http://192.168.0.91/rdv/app/user_control.php";
+    /* String para o banco */
+    private static final String URL = "";
     private StringRequest request;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
         setContentView(R.layout.activity_main);
+
         userEt = findViewById(R.id.etUserName);
         passEt = findViewById(R.id.etPassword);
         login = findViewById(R.id.btnLogin);
         limpar = findViewById(R.id.btnLimpar);
         checkbox = findViewById(R.id.checkBox);
+
+        /*Para recuperar o login da ultima vez que o app foi aberto*/
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String strU = prefs.getString("usr", "");
+        userEt.setText(strU);
 
         checkbox.setOnCheckedChangeListener((compoundButton, isChecked) -> {
             if (isChecked) {
@@ -52,51 +65,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        limpar.setOnClickListener(v->{
+        /* limpar os campos*/
+        limpar.setOnClickListener(v -> {
             userEt.setText("");
             passEt.setText("");
         });
 
-        /*Intent para ir para a outra activity*/
-        Intent i = new Intent(MainActivity.this,Movimento.class);
+        /* Intent para ir para a outra activity */
+        Intent i = new Intent(MainActivity.this, Movimento.class);
 
         requestQueue = Volley.newRequestQueue(this);
-        /*quando clickar em login, chama a função (no php) para login/register */
+        /* quando clickar em login, chama a função (no php) para login/register */
         login.setOnClickListener(v -> {
+            if(userEt.getText().toString().equals("") || passEt.getText().toString().equals("")){
+                Toast.makeText(getApplicationContext(), "Preencha todos os campos!",Toast.LENGTH_SHORT).show();
+            } else {
+                /*para ter o que o login na proxima ver que abrir*/
+                String saveUser = userEt.getText().toString();
 
-            request = new StringRequest(Request.Method.POST, URL, response -> {
-                try {
-                    JSONObject jsonObject = new JSONObject(response);
-                    if(Objects.requireNonNull(jsonObject.names()).get(0).equals("success")){
+                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                SharedPreferences.Editor editor = preferences.edit();
 
-                        Bundle params = new Bundle();
-                        /*Enviar o valor do editText para a próxima activity*/
-                        params.putString("loginUser", userEt.getText().toString());
-                        i.putExtras(params);
-                        /*Assim que passar, vai para a outra activity*/
-                        startActivity(i);
+                editor.putString("usr", saveUser);
+                editor.apply();
+                request = new StringRequest(Request.Method.POST, URL, response -> {
+                    try {
+                        JSONObject jsonObject = new JSONObject(response);
+                        if (Objects.requireNonNull(jsonObject.names()).get(0).equals("success")) {
+                            Bundle params = new Bundle();
+                            /* Enviar o valor do editText para a próxima activity */
+                            params.putString("loginUser", userEt.getText().toString());
+                            i.putExtras(params);
+                            /* Assim que passar, vai para a outra activity */
+                            startActivity(i);
 
-                    } else {
-                            if(Objects.requireNonNull(jsonObject.names()).get(0).equals("error")){
-                                Toast.makeText(getApplicationContext(), "Error" + jsonObject.getString("error"), Toast.LENGTH_SHORT).show();
-                            }
+                        } else {
+                            /* msg de erro por 1.5sec*/
+                            new Handler().postDelayed(() ->
+                                    Toast.makeText(getApplicationContext(), "Erro ao logar",Toast.LENGTH_SHORT).show(), 1500);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
 
-            }, error -> {
+                }, error -> {
 
-            }) {
-                @Override
-                protected Map<String, String> getParams() {
-                    HashMap<String,String> hashMap = new HashMap<>();
-                    hashMap.put("username", userEt.getText().toString());
-                    hashMap.put("password", passEt.getText().toString());
-                    return hashMap;
-                }
-            };
-            requestQueue.add(request);
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() {
+                        HashMap<String, String> hashMap = new HashMap<>();
+                        hashMap.put("username", userEt.getText().toString());
+                        hashMap.put("password", passEt.getText().toString());
+                        return hashMap;
+                    }
+                };
+                requestQueue.add(request);
+            }
+
         });
 
     }

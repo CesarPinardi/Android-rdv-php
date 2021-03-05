595ceebd5e9ce972a8b90ed9b66ffc919ccbb25e
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-03-05 15:34:46 -0300

    Update MainActivity.java

--- main/java/com/example/rdv01/MainActivity.java
+++ main/java/com/example/rdv01/MainActivity.java
@@ -28,6 +28,7 @@ import java.util.Map;
 import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
+
     EditText userEt, passEt;
     Button login;
     Button limpar;
@@ -35,7 +36,8 @@ public class MainActivity extends AppCompatActivity {
 
     private RequestQueue requestQueue;
     /* String para o banco */
-    private static final String URL = "";
+    private static final String URL =
+            "http://189.1.174.107:8080/app/user_control.php";
     private StringRequest request;
 
     @Override
@@ -55,75 +57,86 @@ public class MainActivity extends AppCompatActivity {
         String strU = prefs.getString("usr", "");
         userEt.setText(strU);
 
-        checkbox.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-            if (isChecked) {
-                // mostra password
-                passEt.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
-            } else {
-                // esconde password
-                passEt.setTransformationMethod(PasswordTransformationMethod.getInstance());
-            }
-        });
+        checkbox.setOnCheckedChangeListener(
+                (compoundButton, isChecked) -> {
+                    if (isChecked) {
+                        // mostra password
+                        passEt.setTransformationMethod(
+                                HideReturnsTransformationMethod.getInstance()
+                        );
+                    } else {
+                        // esconde password
+                        passEt.setTransformationMethod(
+                                PasswordTransformationMethod.getInstance()
+                        );
+                    }
+                }
+        );
 
         /* limpar os campos*/
-        limpar.setOnClickListener(v -> {
-            userEt.setText("");
-            passEt.setText("");
-        });
+        limpar.setOnClickListener(
+                v -> {
+                    userEt.setText("");
+                    passEt.setText("");
+                }
+        );
 
         /* Intent para ir para a outra activity */
         Intent i = new Intent(MainActivity.this, Movimento.class);
 
         requestQueue = Volley.newRequestQueue(this);
         /* quando clickar em login, chama a função (no php) para login/register */
-        login.setOnClickListener(v -> {
-            if(userEt.getText().toString().equals("") || passEt.getText().toString().equals("")){
-                Toast.makeText(getApplicationContext(), "Preencha todos os campos!",Toast.LENGTH_SHORT).show();
-            } else {
-                /*para ter o que o login na proxima ver que abrir*/
-                String saveUser = userEt.getText().toString();
+        login.setOnClickListener(
+                v -> {
+                    if (
+                            userEt.getText().toString().equals("") ||
+                                    passEt.getText().toString().equals("")
+                    ) {
+                        Toast.makeText(getApplicationContext(),"Preencha todos os campos!", Toast.LENGTH_SHORT).show();
 
-                SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
-                SharedPreferences.Editor editor = preferences.edit();
+                    } else {
 
-                editor.putString("usr", saveUser);
-                editor.apply();
-                request = new StringRequest(Request.Method.POST, URL, response -> {
-                    try {
-                        JSONObject jsonObject = new JSONObject(response);
-                        if (Objects.requireNonNull(jsonObject.names()).get(0).equals("success")) {
-                            Bundle params = new Bundle();
-                            /* Enviar o valor do editText para a próxima activity */
-                            params.putString("loginUser", userEt.getText().toString());
-                            i.putExtras(params);
-                            /* Assim que passar, vai para a outra activity */
-                            startActivity(i);
+                        /*para ter o que o login na proxima ver que abrir*/
+                        String saveUser = userEt.getText().toString();
 
-                        } else {
-                            /* msg de erro por 1.5sec*/
-                            new Handler().postDelayed(() ->
-                                    Toast.makeText(getApplicationContext(), "Erro ao logar",Toast.LENGTH_SHORT).show(), 1500);
-                        }
-                    } catch (JSONException e) {
-                        e.printStackTrace();
+                        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+                        SharedPreferences.Editor editor = preferences.edit();
+
+                        editor.putString("usr", saveUser);
+                        editor.apply();
+                        request =
+                                new StringRequest(Request.Method.POST, URL, response -> {
+                                    try {
+                                        JSONObject jsonObject = new JSONObject(response);
+                                        if (Objects.requireNonNull(jsonObject.names()).get(0).equals("success")) {
+                                            Bundle params = new Bundle();
+                                            /* Enviar o valor do editText para a próxima activity */
+                                            params.putString("loginUser", userEt.getText().toString());
+                                            i.putExtras(params);
+                                            /* Assim que passar, vai para a outra activity */
+                                            startActivity(i);
+                                        } else {
+                                            /* msg de erro por 1.5sec*/
+                                            new Handler()
+                                                .postDelayed(() -> Toast.makeText(getApplicationContext(),"Erro ao logar",Toast.LENGTH_SHORT).show(), 1500);
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                },
+                                        error -> {}
+                                ) {
+                                    @Override
+                                    protected Map<String, String> getParams() {
+                                        HashMap<String, String> hashMap = new HashMap<>();
+                                        hashMap.put("username", userEt.getText().toString());
+                                        hashMap.put("password", passEt.getText().toString());
+                                        return hashMap;
+                                    }
+                                };
+                        requestQueue.add(request);
                     }
-
-                }, error -> {
-
-                }) {
-                    @Override
-                    protected Map<String, String> getParams() {
-                        HashMap<String, String> hashMap = new HashMap<>();
-                        hashMap.put("username", userEt.getText().toString());
-                        hashMap.put("password", passEt.getText().toString());
-                        return hashMap;
-                    }
-                };
-                requestQueue.add(request);
-            }
-
-        });
-
+                }
+        );
     }
-
 }

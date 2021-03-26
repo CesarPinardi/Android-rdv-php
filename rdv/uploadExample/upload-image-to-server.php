ed4ee5126cb7d06128b41e6c7b5b76ff394c0c25
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-03-26 13:43:27 -0300

    Update upload-image-to-server.php

--- rdv/uploadExample/upload-image-to-server.php
+++ rdv/uploadExample/upload-image-to-server.php
@@ -4,34 +4,51 @@ include 'config.php';
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
- 
- if($_SERVER['REQUEST_METHOD'] == 'POST')
- {
- $DefaultId = 0;
- 
- $ImageData = $_POST['image_data'];
- 
- $ImageName = $_POST['image_tag'];
 
- $ImageUser = $_POST['image_user'];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $DefaultId = 0;
 
- $ImageDesp = $_POST['image_desp'];
- 
- $ImagePath = "uploads/$ImageName.jpg";
- 
- $ServerURL = "uploadExample/$ImagePath";
- 
- $InsertSQL = "INSERT INTO photos (image_path,image_name, image_user, image_desp) 
- values('$ServerURL','$ImageName','$ImageUser','$ImageDesp')";
- 
- if(mysqli_query($conn, $InsertSQL)){
+    $ImageData = $_POST['image_data'];
 
- file_put_contents($ImagePath,base64_decode($ImageData));
+    $ImageName = $_POST['image_tag'];
 
- echo "Sua imagem foi enviada!.";
- }
- 
- mysqli_close($conn);
- }else{
- echo "Por favor, tente novamente.";
- }
+    $Cd_user = $_POST['cd_user'];
+
+    $Direcao = $_POST['direcao'];
+
+    $Gerencia = $_POST['gerencia'];
+
+    $Equipe = $_POST['equipe'];
+
+    $IdDesp = $_POST['id_desp'];
+
+    $ValorDesp = $_POST['valor_desp'];
+
+    $Km = $_POST['km'];
+
+    $Obs = $_POST['obs'];
+
+    $Data = $_POST['dataM'];
+
+    $Status = $_POST['statusM'];
+
+    $ObsRep = $_POST['obsRep'];
+
+    $ImagePath = "uploads/$ImageName.jpg";
+
+    $ServerURL = "uploadExample/uploads/$ImageName.jpg";
+
+    $InsertSQL = "INSERT INTO photos (image_path,image_name, cd_user, direcao, gerencia, equipe, id_desp, valor_desp, km, obs, dataM, statusM, obsRep) 
+ values('$ServerURL','$ImageName','$Cd_user', '$Direcao', '$Gerencia', '$Equipe', '$IdDesp', '$ValorDesp', '$Km', '$Obs', '$Data', '$Status', '$ObsRep')";
+
+    if (mysqli_query($conn, $InsertSQL)) {
+
+        file_put_contents($ImagePath, base64_decode($ImageData));
+
+        echo "Sua imagem foi enviada!";
+    }
+
+    mysqli_close($conn);
+} else {
+    echo "Por favor, tente novamente.";
+}

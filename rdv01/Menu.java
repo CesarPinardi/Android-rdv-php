77c74903671e3415dc654a93a8ac759e8183f632
César Augusto Pinardi <cesar.pinardi@gmail.com>
2021-06-17 16:50:28 -0300

    v3

--- rdv01/Menu.java
+++ rdv01/Menu.java
@@ -1,22 +1,41 @@
-package com.controll_rdv.rdv01;
+package com.controll_rdv.rdv03;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class Menu extends AppCompatActivity {
 
     TextView user;
     int classe;
     String getGerenc, getEquip, getDir;
+    ListView lv;
+    ArrayList<String> holder = new ArrayList<>();
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +43,8 @@ public class Menu extends AppCompatActivity {
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         /*declarações de variaveis */
+
+        lv = findViewById(R.id.lv);
 
         user = findViewById(R.id.tvnomeMenu);
         pegarDados();
@@ -36,8 +57,100 @@ public class Menu extends AppCompatActivity {
         String strU = prefs.getString("usr", "");
         user.setText(strU);
         recuperarVoltar();
+        getDespesa();
 
     }
+
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void getDespesa() {
+
+        String apiurl = getString(R.string.getGastos) + "beto";
+        @SuppressLint("StaticFieldLeak")
+        class dbManager extends AsyncTask<String, Void, String> {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            protected void onPostExecute(String data) {
+                try {
+                    JSONArray ja = new JSONArray(data);
+                    JSONObject jo;
+                    holder.clear();
+                    for (int i = 0; i < ja.length(); i++) {
+                        jo = ja.getJSONObject(i);
+
+                        String despesa = jo.getString("id_desp");
+                        String total = jo.getString("sum(valor_desp)");
+
+                        String tipoDesp;
+                        switch (Integer.parseInt(despesa)) {
+                            case 1:
+                                tipoDesp = "Alimentação";
+                                break;
+                            case 2:
+                                tipoDesp = "Combustível";
+                                break;
+                            case 3:
+                                tipoDesp = "Estacionamento";
+                                break;
+                            case 4:
+                                tipoDesp = "Hospedagem";
+                                break;
+                            case 5:
+                                tipoDesp = "Outros";
+                                break;
+                            case 6:
+                                tipoDesp = "Pedágio";
+                                break;
+                            default:
+                                throw new IllegalStateException("Unexpected value: " + Integer.parseInt(despesa));
+                        }
+
+                        holder.add(tipoDesp + ": R$" + total);
+
+                    }
+
+                    ArrayAdapter<String> at = new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, holder);
+
+                    lv.setAdapter(at);
+
+                } catch (Exception e) {
+                    System.out.print(e.getMessage());
+                    Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    //finish();
+                }
+            }
+
+            @Override
+            protected String doInBackground(String... strings) {
+
+                try {
+                    URL url = new URL(strings[0]);
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+
+                    StringBuilder data = new StringBuilder();
+                    String line;
+
+                    while ((line = br.readLine()) != null) {
+                        data.append(line).append("\n");
+                    }
+                    br.close();
+
+                    return data.toString();
+
+                } catch (IOException e) {
+                    return e.getMessage();
+                }
+
+            }
+
+
+        }
+
+        dbManager obj = new dbManager();
+        obj.execute(apiurl);
+
+    }
+
 
     private void recuperarVoltar() {
         /*recuperando o username a partir da funcao sharedpreferences*/
